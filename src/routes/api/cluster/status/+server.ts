8dd1908d2a761403c509243d9dcd5f2e9ccb0e31
Jakub Coufal <jakub.coufal@jamf.com>
2023-12-20 14:42:37 +0100

    add: improve cluster API

--- src/routes/api/cluster/status/+server.ts
+++ src/routes/api/cluster/status/+server.ts
@@ -1,22 +1,9 @@
-import { json, type RequestHandler } from '@sveltejs/kit';
-import type { MemberListResponse } from '$lib/proto/regatta/v1/MemberListResponse';
-import type { StatusResponse } from '$lib/proto/regatta/v1/StatusResponse';
-import type { Member } from '$lib/proto/regatta/v1/Member';
+import { error, json, type RequestHandler } from '@sveltejs/kit';
 import { cluster } from '$lib/server/cluster';
 
-interface MemberStatus extends Member {
-	status: StatusResponse;
-}
-
-export const GET: RequestHandler = async (): Promise<Response> => {
-	const ml: MemberListResponse = await cluster.memberList({});
-	const status: MemberStatus[] = [];
-	const members = ml.members || [];
-	for (let i = 0; i < members.length; i++) {
-		const clientURLs = members[i].clientURLs || [];
-		const st = await cluster.status({}, clientURLs[0].split('://')[1]);
-		status.push({ ...members[i], status: st });
+export const GET: RequestHandler = async ({ url }): Promise<Response> => {
+	if (!url.searchParams.has('target')) {
+		error(412, 'must provide target query parameter');
 	}
-	// @ts-expect-error type err
-	return json(status.sort((a, b) => a.id - b.id));
+	return json(await cluster.status({}, url.searchParams.get('target')!));
 };
--- src/routes/cluster/+page.svelte
+++ src/routes/cluster/+page.svelte
@@ -5,18 +5,18 @@
 
 <h3 class="h3">ClusterNodes</h3>
 <div class="container grid grid-cols-1 md:grid-cols-2 gap-4">
-	{#each data.status ?? [] as node}
+	{#each data.members ?? [] as member}
 		<div class="card">
 			<header class="card-header">
-				<div class="text-xs opacity-50">Node: {node.name}</div>
+				<div class="text-xs opacity-50">Node: {member.name}</div>
 			</header>
 			<section class="p-4">
-				<p class="text-xs">Id: {node.id}</p>
-				<p class="text-xs">Peer: {node.peerURLs}</p>
-				<p class="text-xs">ClientURL: {node.clientURLs}</p>
+				<p class="text-xs">Id: {member.id}</p>
+				<p class="text-xs">Peer: {member.peerURLs}</p>
+				<p class="text-xs">ClientURL: {member.clientURLs}</p>
 			</section>
 			<footer class="card-footer">
-				<p class="text-xs">Version: {node.status.version}</p>
+				<p class="text-xs">Version: {data.statuses.get(member.id).version}</p>
 			</footer>
 		</div>
 	{/each}
