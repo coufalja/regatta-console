06ae437058fe88ee6ed9f8215c22a5dd15cdcf57
Jakub Coufal <jakub.coufal@jamf.com>
2023-12-26 09:08:16 +0100

    add: use API endpoint instead of lib direct in query

--- src/routes/query/+page.server.js
+++ src/routes/query/+page.server.js
@@ -1,24 +1,21 @@
-import { kv } from '$lib/server/kv';
+import { error } from '@sveltejs/kit';
 
 /** @type {import('./$types').Actions} */
 export const actions = {
-	query: async ({ request }) => {
+	query: async ({ request, fetch }) => {
 		const data = await request.formData();
 		const table = data.get('table') + '';
 		const key = data.get('query') + '';
-		const req = {
-			table,
-			key
-		};
-		try {
-			const response = await kv.range(req);
-			return { request: req, response, error: undefined };
-		} catch (err) {
-			let error = 'unknown';
-			if (err instanceof Error) {
-				error = err.message;
-			}
-			return { request: req, response: undefined, error };
+		const resp = await fetch(`/api/query`, {
+			method: 'POST',
+			body: JSON.stringify({
+				table,
+				key
+			})
+		});
+		if (!resp.ok) {
+			error(500, 'backend query error');
 		}
+		return await resp.json();
 	}
 };
