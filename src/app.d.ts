d50e91aaa7ecdfc74926ec02196e4dedfb4cef78
Jakub Coufal <jakub.coufal@jamf.com>
2024-01-28 10:56:05 +0100

    add: session typing

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,11 @@
 import 'unplugin-icons/types/svelte';
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
-// and what to do when importing types
+import type { Session } from '@auth/sveltekit';
+
 declare namespace App {
-	// interface Locals {}
+	interface Locals {
+		session?: Session;
+	}
+
 	// interface PageData {}
 	// interface Error {}
 	// interface Platform {}
